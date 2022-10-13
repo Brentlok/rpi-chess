0442f7db1419412cd54f3f34f6766b4b21b3ee87
Brentlok <brentlokk@gmail.com>
2022-10-13 21:16:00 +0200

    Add matrix

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 console.clear();
 const jsChessEngine = require('js-chess-engine');
+const Max7219 = require('max7219-display');
 
 type LetterType = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
 type NumberType = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
@@ -25,3 +26,27 @@ const aiMove = () => {
     const [{ from, to }] = game.getHistory('reversed');
     return { from, to };
 }
+
+const matrix = new Max7219({ 
+    device: '/dev/spidev0.0',
+    controllerCount: 1, 
+    flip: 'vertical'
+});
+
+const myMatrix = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,1,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,1,0],
+    [0,1,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+];
+  
+const init = async () => {
+    await matrix.reset(0);
+    await matrix.set(0, myMatrix);
+}
+
+init();
